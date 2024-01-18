43ad0c0e590d96b5511bd6cfdea7a0f50bb1b81d
shen <1779841069@qq.com>
2024-01-18 17:58:22 +0800

    Update wei-bo.js

--- wei-bo.js
+++ wei-bo.js
@@ -7,8 +7,7 @@ if (-1 != $request.url.indexOf("container_timeline")) {
    if (t.items && t.items.length > 0 &&
         t.items[0].items && t.items[0].items.length > 0 &&
         t.items[0].items[0].data && t.items[0].items[0].data.group) {
-      t.data = t.data.filter(t => !("sponsorCard" == t.entityTemplate || -1 != t.title.indexOf("精选配件") ||"imageSquareScrollCard" == t.entityTemplate ))
-        // 应用新的过滤条件
+         // 应用新的过滤条件
         t.items[0].items[0].data.group = t.items[0].items[0].data.group.filter(item => !item.hasOwnProperty('promotion') || !item.hasOwnProperty('icon') );
         $done({
             body: JSON.stringify(t)

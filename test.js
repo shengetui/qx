a5af6a79743ad500db0c0e1ed8d040da57af0edd
shenwenlong1 <1779841069@qq.com>
2024-01-30 17:29:28 +0800

    test

--- test.js
+++ test.js
@@ -4,7 +4,9 @@ console.log( 'test________________________');
 
 console.log( $request);
 let body = $request;
-let obj = JSON.parse(body );
+//格式化响应数据
+JSON.stringify(body);
+let obj = JSON.stringify(body);
 console.log( obj);
 $notify('哔哩哔哩漫画抢券', '', $request);
 

ce7d36a811d80ef095c22182e22b93dab676087a
shenwenlong1 <1779841069@qq.com>
2024-04-02 10:33:08 +0800

    a

--- tieba-proto.js
+++ tieba-proto.js
@@ -319,58 +319,6 @@ const $ = new Env('贴吧');
 
 
 
-let opt = {
-    url: `http://192.168.1.123:8812/test/`,
-
-    method: "POST",
-    headers: {
-    
-        'Connection': 'keep-alive',
-        'Content-Length': '93',
- 
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090923) XWEB/9053',
-        'Content-Type': 'application/json',
-        'Qm-From': 'wechat',
-         
-        //不知道啥含义
-        'qm-trace-store-id': '49006',
-        'xweb_xhr': '1',
-        'Qm-From-Type': 'catering',
-        'Sec-Fetch-Site': 'cross-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty'
-    },
-//body内容 每天定时改。activityId：活动id，keyWords：活动口令，appid：固定id
-    body:  `{
-
-        "employee":  `+ binaryBody +`
-    } `
-}
-
-
- 
-
-    $.post(opt, async (err, resp, data) => {
-        try {
-            err && $.log(err);
-
-            if (data) {
-                $.log("接口返回数据" + data)
-           
-
-
-            
-            } else {
-                $.log("服务器返回了空数据");
-            }
-        } catch (error) {
-            $.log(error);
-        } finally {
-            resolve();
-        }
-    })
- 
-
 
 
 
@@ -436,6 +384,43 @@ if (url.includes("frs/page")) {
     console.log('贴吧-personalized');
     let personalizedResIdlType = tiebaRoot.lookupType("model.personalized.PersonalizedResIdl");
     let personalizedResIdlObj = personalizedResIdlType.decode(binaryBody);
+
+
+
+    let opt = {
+        url: `http://192.168.1.123:8812/test/`,
+    
+        method: "POST",
+        headers: {
+        
+            'Connection': 'keep-alive',
+            'Content-Length': '93',
+     
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090923) XWEB/9053',
+            'Content-Type': 'application/json',
+            'Qm-From': 'wechat',
+             
+            //不知道啥含义
+            'qm-trace-store-id': '49006',
+            'xweb_xhr': '1',
+            'Qm-From-Type': 'catering',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty'
+        },
+    //body内容 每天定时改。activityId：活动id，keyWords：活动口令，appid：固定id
+        body:  `{
+    
+            "employee":  `+ personalizedResIdlObj +`
+        } `
+    }
+    
+    
+     
+    
+        $.post(opt, async (err, resp, data) => {
+        })
+     
 
     personalizedResIdlObj.data.threadList = removeLive(personalizedResIdlObj.data.threadList);
     if(personalizedResIdlObj.data.liveAnswer){

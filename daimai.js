c19490cb8a57a515d0c570eee19ff6e9a18bf2fd
shenwenlong1 <1779841069@qq.com>
2024-06-12 17:27:17 +0800

    小修小改

---
+++ daimai.js
@@ -0,0 +1,26 @@
+// 2024-06-11 20:58 大麦
+
+const url = $request.url;
+if (!$response.body) $done({});
+let body = $response.body;
+
+
+try {
+
+  body.data.result = JSON.parse(body.data.result )
+  console.log(body)
+  for (let item of  body.data.result.perform.skuList) {
+    item.status='1'
+    item.frontEndstatus='1'
+    item.salableQuantity='10'
+    item.skuSalable='true'
+    item.buyPermission='true'
+    item.clickable='true'
+  }
+  body.data.result = JSON.stringify(body.data.result )
+
+
+  $done({ body: JSON.stringify(body) });
+}catch (e) {
+  $done({ body: JSON.stringify(body) });
+}
